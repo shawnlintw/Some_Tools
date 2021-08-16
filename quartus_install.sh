b95708acbb4e5a1ed53ccd870472799c2a2fe7a4
shawnlintw <edwin2619+github@gmail.com>
2021-08-16 12:02:36 +0800

    Update quartus_install.sh
    add -y argument

--- quartus_install.sh
+++ quartus_install.sh
@@ -1,8 +1,8 @@
 	 sudo dpkg --add-architecture i386
 	 sudo add-apt-repository -y ppa:linuxuprising/libpng12
 	 sudo apt update
-	 sudo apt upgrade
-	 sudo apt-get install build-essential
+	 sudo apt -y upgrade
+	 sudo apt-get install -y build-essential
 	 sudo apt install -y libc6:i386 libncurses5:i386 libxtst6:i386 libxft2:i386 libstdc++6:i386 libc6-dev-i386 lib32z1 libncurses5 libbz2-1.0:i386 libpng12-0 libqt5xml5 liblzma-dev libcanberra-gtk-module
 	 sudo apt-get install -y libcanberra-gtk-module
 	 cd ~/Downloads/
